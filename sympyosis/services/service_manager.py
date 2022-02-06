103b4e0c467ddebf04a5a0cb0f9ea8a6e6465342
Zech Zimmerman <hi@zech.codes>
2022-02-06 15:37:15 -0500

    Run services using asyncio.gather

--- sympyosis/services/service_manager.py
+++ sympyosis/services/service_manager.py
@@ -5,6 +5,8 @@ from sympyosis.exceptions import BaseSympyosisException
 from sympyosis.logger import Logger
 from sympyosis.services import Service
 from sympyosis.services.interface_provider import InterfaceProviderProtocol
+from typing import Awaitable
+import asyncio
 
 
 class SympyosisUnableToFindStartCoroutine(BaseSympyosisException):
@@ -22,8 +24,28 @@ class ServiceManager(AutoInject):
 
     async def start(self):
         self._create_services()
+        await self._run_services()
+
     def _create_services(self):
         for config in self.config.services.values():
             service = self.service_builder(config)
             self.log.info(f"Creating {service.name!r} service")
             self._services[service.name] = service.create_interface()
+
+    async def _run_services(self):
+        funcs: list[Awaitable] = []
+        for name, service in self._services.items():
+            try:
+                funcs.append(service.start())
+            except AttributeError:
+                self.log.error(
+                    f"Could not find the start coroutine on the {name!r} service's provider "
+                    f"{type(service).__qualname__!r}."
+                )
+                raise SympyosisUnableToFindStartCoroutine(
+                    f"Could not find the start coroutine on the {name!r} service's provider "
+                    f"{type(service).__qualname__!r}.\n\nBe sure that the service's interface module and interface "
+                    f"class are correct in the Sympyosis config file. The services provider class must adhere to the "
+                    f"interface protocol ({InterfaceProviderProtocol.__qualname__})."
+                )
+        await asyncio.gather(*funcs)
