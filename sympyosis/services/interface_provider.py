c9d2e454834e4dce1857546ba0afc1d13500a877
Zech Zimmerman <hi@zech.codes>
2022-02-06 15:33:50 -0500

    Add start coroutine to interface provider protocol

--- sympyosis/services/interface_provider.py
+++ sympyosis/services/interface_provider.py
@@ -3,3 +3,6 @@ from typing import Protocol
 
 class InterfaceProviderProtocol(Protocol):
     """Protocol that all interface providers need to implement."""
+
+    async def start(self):
+        ...
