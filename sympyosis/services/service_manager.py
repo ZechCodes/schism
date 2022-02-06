1ad35527352f22dadb5df6aa68f1cba985873caf
Zech Zimmerman <hi@zech.codes>
2022-02-06 12:12:28 -0500

    Add some logging in the service manager

--- sympyosis/services/service_manager.py
+++ sympyosis/services/service_manager.py
@@ -1,12 +1,14 @@
 from bevy import AutoInject, detect_dependencies
 from bevy.builder import Builder
 from sympyosis.config import Config
+from sympyosis.logger import Logger
 from sympyosis.services import Service
 
 
 @detect_dependencies
 class ServiceManager(AutoInject):
     config: Config
+    log: Logger
     service_builder: Builder[Service]
 
     def start(self):
@@ -15,4 +17,5 @@ class ServiceManager(AutoInject):
     def _start_services(self):
         for config in self.config.services.values():
             service = self.service_builder(config)
+            self.log.info(f"Creating {service.name!r} service")
             service.create_interface()
