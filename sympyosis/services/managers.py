d9e0a0ed848919905d62d13f02bd109094f4fd67
Zech Zimmerman <hi@zech.codes>
2022-02-09 14:36:34 -0500

    Make the service manager protocol runtime checkable

--- sympyosis/services/managers.py
+++ sympyosis/services/managers.py
@@ -1,11 +1,12 @@
 from sympyosis.exceptions import BaseSympyosisException
-from typing import Protocol
+from typing import Protocol, runtime_checkable
 
 
 class SympyosisUnableToFindStartCoroutine(BaseSympyosisException):
     """Raised when Sympyosis cannot find the start coroutine on a service's interface."""
 
 
+@runtime_checkable
 class ServiceManager(Protocol):
     async def start(self):
         ...
