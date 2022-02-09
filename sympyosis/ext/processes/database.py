5057b74f5606cc9ebd126fb67783a678a89c0c23
Zech Zimmerman <hi@zech.codes>
2022-02-09 16:22:15 -0500

    Add a simple SQLite db for process tracking

---
+++ sympyosis/ext/processes/database.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+from bevy import AutoInject, detect_dependencies
+from enum import IntEnum, auto
+from pathlib import Path
+from sympyosis import Options
+import sqlite3
+
+
+class ProcessStatus(IntEnum):
+    RUNNING = auto()
+    STARTING = auto()
+    STOPPED = auto()
+    STOPPING = auto()
+    DELETED = auto()
+
+
+class ProcessModel:
+    def __init__(
+        self, service: str, pid: int, status: ProcessStatus | int, *, db: Database
+    ):
+        self._service = service
+        self._pid = pid
+        self._status = ProcessStatus(status)
+        self._db = db
+
+    @property
+    def service(self) -> str:
+        return self._service
+
+    @property
+    def pid(self) -> int:
+        return self._pid
+
+    @property
+    def status(self) -> ProcessStatus:
+        return self._status
+
+    @status.setter
+    def status(self, status: ProcessStatus):
+        self._db.update_process_status(self.pid, status)
+        self._status = status
+
+    def delete(self):
+        self._db.delete_process(self.pid)
+        self._status = ProcessStatus.DELETED
+
+    def __repr__(self):
+        return f"{type(self).__name__}(service={self.service!r}, pid={self.pid}, status={self.status})"
+
+
+@detect_dependencies
+class Database(AutoInject):
+    options: Options
+
+    def __init__(self):
+        self.db = self._get_database()
+
+    @property
+    def version(self) -> int:
+        query = "SELECT * FROM Metadata WHERE key == 'DB_VERSION'"
+        row = self.db.execute(query).fetchone()
+        return int(row[1])
+
+    @property
+    def file_path(self) -> Path:
+        file_name = self.options.get("PROCLIST_FILE_NAME", "sympyosis_proclist")
+        return Path(self.options[self.options.path_option_name]) / file_name
+
+    def add_process(self, service: str, pid: int, status: ProcessStatus):
+        self.db.execute(
+            "INSERT INTO Processes(service, pid, status) VALUES(?, ?, ?)",
+            (service, pid, status.value),
+        )
+        self.db.commit()
+
+    def get_processes(self, status: ProcessStatus | None = None) -> list[ProcessModel]:
+        query = "SELECT * FROM Processes"
+        parameters = tuple()
+        if status is not None:
+            query += " WHERE status = ?"
+            parameters = (status.value,)
+
+        return [
+            ProcessModel(*row, db=self) for row in self.db.execute(query, parameters)
+        ]
+
+    def update_process_status(self, pid: int, status: ProcessStatus):
+        self.db.execute(
+            "UPDATE Processes SET status=? WHERE pid=?", (status.value, pid)
+        )
+        self.db.commit()
+
+    def delete_process(self, pid: int):
+        self.db.execute("DELETE FROM Processes WHERE pid=?", (pid,))
+        self.db.commit()
+
+    def _build_database(self, db: sqlite3.Connection):
+        db.execute(
+            "CREATE TABLE Processes("
+            "    service VARCHAR(255),"
+            "    pid INTEGER,"
+            "    status INTEGER"
+            ");"
+        )
+
+        db.execute("CREATE TABLE Metadata(key VARCHAR(64), value VARCHAR(256));")
+        db.execute("INSERT INTO Metadata(key, value) VALUES('DB_VERSION', '0')")
+        db.commit()
+
+    def _connect(self) -> sqlite3.Connection:
+        return sqlite3.connect(self.file_path.resolve())
+
+    def _get_database(self) -> sqlite3.Connection:
+        exists = self.file_path.exists()
+        db = self._connect()
+
+        if not exists:
+            self._build_database(db)
+
+        return db
---
+++ tests/test_ext_processes.py
@@ -0,0 +1,34 @@
+from sympyosis.ext.processes.database import Database, ProcessStatus
+import sqlite3
+
+
+class MemoryDB(Database):
+    def __init__(self):
+        self.db = sqlite3.connect(":memory:")
+        self._build_database(self.db)
+
+
+def test_db_version():
+    db = MemoryDB()
+    assert db.version == 0
+
+
+def test_get_processes():
+    db = MemoryDB()
+    db.add_process("Test 1", 0, ProcessStatus.STOPPED)
+    db.add_process("Test 2", 1, ProcessStatus.RUNNING)
+
+    assert len(db.get_processes()) == 2
+    assert len(db.get_processes(ProcessStatus.RUNNING)) == 1
+
+
+def test_delete_processes():
+    db = MemoryDB()
+    db.add_process("Test 1", 0, ProcessStatus.RUNNING)
+
+    process = db.get_processes()[0]
+    assert process.status == ProcessStatus.RUNNING
+
+    process.delete()
+    assert process.status == ProcessStatus.DELETED
+    assert len(db.get_processes()) == 0
